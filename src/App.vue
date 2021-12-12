6c0f3428d137be0867aea777d5e1ea011dc299a5
hhakangull <hakanngul@icloud.com>
2021-12-12 22:43:40 +0300

    v-slot-detayli

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,14 @@
 <template>
-  <div class="container py-5 text-center">
+  <div class="container py-5">
     <h1 class="mt-5">ANA SAYFA</h1>
-    <h2>APP VUE HOME Component İşlemleri</h2> <hr>
-    <vierone-component class="text-danger" />
+    <h2>APP VUE HOME Component İşlemleri</h2>
+    <hr />
+    <vierone-component>
+      <template #header>hepsiburada rezaleti</template>
+      <template #body>Merhabalar bu veri ana vue den geldi</template>
+
+      <template #footer>yazar: vierone 1 saat önce</template>
+    </vierone-component>
     <hr />
   </div>
 </template>
--- src/components/Vierone.vue
+++ src/components/Vierone.vue
@@ -1,19 +1,44 @@
-<template inherit-attrs="true">
-   
-      <h1>Vierone.vue Dosyasıdır</h1>
-      <hr />
-      <div v-bind="$attrs">
-       {{ msg }} -- {{$attrs}}
+<template>
+   <div class="card">
+      <div class="card-element card-header h3 m-0 text-uppercase">
+      <slot name="header"/>
       </div>
-      <hr />
-      <button class="btn btn-primary" @click="testFunction">Tıkla Bana</button>
- 
+      <div class="card-element card-body">
+      <slot name="body"/>
+      </div>
+      <div class="card-element card-footer small">
+      <slot name="footer"/>
+      </div>
+   </div>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue';
-const msg = ref("bu vierone componentinen bir değerdir");
-const testFunction = () => { console.log("Vierone Componentineden merhabalar"); }
 
 
 </script>
+
+<style lang="scss" scoped>
+.card{
+   border:1px solid rgb(173, 171, 171);
+   box-shadow: 0px 0px 6px 1px rgb(190, 190, 190);
+   border-radius: 10px;
+   font-family: Arial, Helvetica, sans-serif;
+   max-width: 500px;
+   margin: auto;
+}
+.card-element {
+   padding: 5px;
+}
+.card-header {
+   text-align: center;
+   border-bottom: 1px solid rgb(199, 199, 199);
+   padding: 15px 10px;
+}
+.card-body{
+   padding: 15px;
+}
+.card-footer{
+   padding:15px
+}
+</style>
