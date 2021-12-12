aa39e92ac0d4761cfeb9c1eaacd709c698fb9b94
hhakangull <hakanngul@icloud.com>
2021-12-12 12:37:28 +0300

    template islemleri 2

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,35 @@
 <template>
-  <div class="container py-5">
-    <h3 class="mt-5">ANA SAYFA</h3>
-    <div class="col-sm-10">{{ values }}</div>
+  <div class="container py-5 text-center">
+    <h1 class="mt-5">ANA SAYFA</h1>
+    <h2>Template İşlemleri</h2>
+    <h3>{{values}}</h3>
+    <hr />
     <div>
-      <div class="form-check">
-        <input v-model="values" class="form-check-input" type="radio" value="araba" />
-        <label class="form-check-label" for="gridRadios1">Araba</label>
-      </div>
-      <div class="form-check">
-        <input v-model="values" class="form-check-input" type="radio" value="motor" />
-        <label class="form-check-label" for="gridRadios2">Motor</label>
-      </div>
-      <div class="form-check">
-        <input v-model="values" class="form-check-input" type="radio" value="kamyonet" />
-        <label class="form-check-label" for="gridRadios2">Kamyonet</label>
-      </div>
+      <p class="text-uppercase text-white   h2"
+      :class="{ 'bg-primary': isFirst, 'bg-success': !isFirst }"
+      >
+      {{isFirst ? "Sitemize Hoşgeldiniz" : "Hoşgeldiniz"}}
+      </p>
     </div>
-    <div>{{ checkValues }}</div>
-    <select v-model="checkValues" multiple class="custom-select mr-sm-2">
-      <option value="Araba">Araba</option>
-      <option value="Motor">Motor</option>
-      <option value="Kamyonet">Kamyonet</option>
-    </select> <hr>
-    <select v-model="checkValuesMultiple" class="custom-select mr-sm-2">
-      <option value="Araba">Araba</option>
-      <option value="Motor">Motor</option>
-      <option value="Kamyonet">Kamyonet</option>
-    </select> <hr>
-    <div>
-    <input type="text" v-model.trim="valuesNormal"> <hr>
-    <input type="number" v-model.number="num">  <hr>
-    <input type="valuesNormal" v-model.lazy="valuesNormal">
-    </div>
+    <hr />
+    <button class="btn btn-block  w-50" 
+    :class="{'btn-primary' : isFirst, 'btn-success': !isFirst}"
+    @click="isFirst = !isFirst">{{ isFirst }}</button> <hr>
+  <button class="btn btn-danger" @click="doIt">Değer Ver</button> <hr>
+
   </div>
+  
+
+
 </template>
 <script setup lang="ts">
 import { Ref, ref } from 'vue';
-const values: Ref<String[]> = ref([]);
-const checkValuesMultiple: Ref<String[]> = ref([]);
-const checkValues = ref('');
-
-const valuesNormal = ref('');
+const values = ref('Hello World');
 const num = ref(0);
+const isFirst = ref(true);
+
+const doIt =() =>  values.value = prompt('Değer Ver')!;
+
 
 </script>
 
