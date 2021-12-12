0695f6b1eb137b333bc7740a518572e77ad00434
hhakangull <hakanngul@icloud.com>
2021-12-12 22:53:10 +0300

    propr-icons-with-slots

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,18 @@
 <template>
-  <div class="container py-5">
-    <h1 class="mt-5">ANA SAYFA</h1>
-    <h2>APP VUE HOME Component İşlemleri</h2>
-    <hr />
-    <vierone-component>
-      <template #header>hepsiburada rezaleti</template>
-      <template #body>Merhabalar bu veri ana vue den geldi</template>
-
-      <template #footer>yazar: vierone 1 saat önce</template>
-    </vierone-component>
-    <hr />
-  </div>
+  <Icon>delete</Icon>
+  <Icon>pause</Icon> 
+  <Icon>play_circle</Icon>
 </template>
 <script setup lang="ts">
 import { Ref, ref } from 'vue';
-import VieroneComponent from './components/Vierone.vue';
-
+import Icon from './components/Icon.vue';
 
 
 </script>
 
 <style lang="scss">
 @import "./assets/bootstrap.css";
-
+@import url("https://fonts.googleapis.com/icon?family=Material+Icons");
 .buttonum {
   position: absolute;
 }
