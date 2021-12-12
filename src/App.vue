03580e0234d46b4d87fec94c39ac342119dc2ef1
hhakangull <hakanngul@icloud.com>
2021-12-13 01:05:26 +0300

    tt

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,32 @@
 <template>
-  <div class="mx-auto p-2" style="max-width: 600px;">
-    <div v-if="toggle" class="alert alert-danger" role="alert">Lütfen Giriş Yapın</div>
+  <div class="mx-auto p-2" style="max-width: 600px">
+    <div v-if="toggle" class="alert alert-danger" role="alert">
+      Lütfen Giriş Yapınız!!!      
+    </div>
 
-    <!-- <button class="btn btn-primary" @click="toggle = !toggle">Toogle</button> -->
-    <hr>
-    <ToggleButton @toggle="toggle= $event" />
+    <ToggleButton @toggle="toggleIt($event)">Toggle</ToggleButton>
   </div>
 </template>
-<script setup lang="ts">
-import ToggleButton from './components/ToggleButton.vue';
-import { ref } from 'vue';
-const toggle = ref(true)
+
+<script lang="ts">
+import ToggleButton from "./components/ToggleButton.vue";
+import { defineComponent } from "vue";
+export default defineComponent({
+  data() {
+    return {
+      toggle: true,
+    };
+  },
+  components: {
+    ToggleButton,
+  },
+  methods: {
+    toggleIt(value: boolean) {
+      this.toggle = value;
+    },
+  },
+  
+});
 </script>
 
 <style lang="scss">
--- src/components/ToggleButton.vue
+++ src/components/ToggleButton.vue
@@ -2,6 +2,55 @@
    <button @click="emitEvent" class="btn btn-primary">Toggle</button>
 </template>
 
+
+<!--script-- lang="ts">
+import { h } from 'vue';
+
+import { ref, onMounted, defineEmits, defineComponent } from 'vue'
+export default defineComponent({
+   emits: {
+      toggle: (value: boolean) => true
+   },
+   setup() {
+      const toggle = ref(true);
+      const emit = defineEmits({
+         toggle: (value: boolean) => true,
+      })
+      const emitEvent = () => {
+         toggle.value = !toggle.value;
+         context.emit('toggle', toggle.value)
+      }
+
+      onMounted(() => {
+         emit('toggle', true)
+      })
+
+      return {
+         ... $
+      }
+
+   },
+
+   // data: () => ({
+   //    toggle: true
+   // }),
+   // emits: {
+   //    toggle: (value: boolean) => true
+   // },
+   // methods: {
+   //    emitEvent() {
+   //       this.toggle = !this.toggle;
+   //       this.$emit('toggle', this.toggle)
+   //    },
+   // },
+   // mounted() {
+   //    this.$emit('toggle', true)
+   // }
+
+});
+
+</!--script-->
+
 <script lang="ts" setup>
 import { onMounted, ref } from "vue";
 const toggle = ref(true);
