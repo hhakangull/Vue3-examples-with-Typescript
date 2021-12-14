6cbd9b20f2c665f0ed3bef97b2d8ac52ba841caa
hhakangull <hakanngul@icloud.com>
2021-12-14 23:13:44 +0300

    dinamik class

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,71 @@
 <template>
   <div class="main">
-    <MyComponentVue />
-    <br>
-    <button @click="changeName">İsmi Değiştir</button>
-    
-    
+  <div :class="{active: activeIndex == 0, notActive: activeIndex != 0}" 
+  
+  @click="activeIndex = 0" style="background-color: red; flex:1"></div>  
+  <div :class="{active: activeIndex == 1, notActive: activeIndex != 1}" 
+  @click="activeIndex = 1" style="background-color: orange; flex:1"></div>
+  <div :class="{active: activeIndex == 2, notActive: activeIndex != 2}" 
+  @click="activeIndex = 2" style="background-color: purple; flex:1"></div>
+  
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref, reactive } from "vue";
-import MyComponentVue from "./components/MyComponent.vue";
-import store from './store/store';
+import { ref,Ref } from 'vue';
+
+// activeIndex is ref type null or number
+let activeIndex = ref(-1)
 
 
-
-
-
-// objecler için reactive kullanılır.
-const person = reactive({
-  isim : 'Hakan',
-  soyad: 'GUL',
-  yas: 25
-})
-const changeName = () => {
-  const input = prompt("Yeni İsmi Giriniz");
-  store.isim = input!;
-}
 
 </script>
 
 <style lang="scss">
 @import "./assets/bootstrap.css";
 @import url("https://fonts.googleapis.com/icon?family=Material+Icons");
-.buttonum {
-  position: absolute;
+
+.active {
+  animation: active .5s infinite;
 }
-body {
-  margin: 0;
-  padding: 0;
+.notActive {
+   animation: active .5s infinite;
 }
+
+@keyframes active {
+  0% {
+    transform: scale(1);
+  }
+  50% {
+    transform: scale(2);
+  }
+  100% {
+    transform: scale(1);
+  }
+}
+@keyframes notActive {
+  0% {
+    opacity: 0;
+  }
+  50% {
+    opacity: 1;
+  }
+  100% {
+    opacity: 0;
+  }
+}
+
+
 .main {
-  height: 100vh;
-  width: 100%;
+  background-color: black;
+  // height: 100vh;
+  // width: 100%;
+  margin: 50px;
   display: flex;
-  flex-direction: column;
+  align-items: stretch;
+  
+  // flex-direction: column;
+  width: 600px;
+  height: 300px;
 }
 </style>
