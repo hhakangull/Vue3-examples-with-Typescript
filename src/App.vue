06cb896923d67bfa9b921231a882c76e1c46c683
hhakangull <hakanngul@icloud.com>
2021-12-15 23:54:59 +0300

    render

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,56 @@
+<template>
+  <div class="main">
+    <button @click="num++" class="btn btn-primary">Click Me</button>
+    <div class="h1 mt-3">{{ num }}</div>
 
-<script  lang="ts">
-import { defineComponent, h, ref } from "vue";
+    <button @click="input">Yazı Gir</button>
+    <div>{{ mesaj }}</div>
+  </div>
+</template>
 
-export default defineComponent({
-  setup() {
-    const myEl = ref(2);
-    const log = () => console.log("Selem Render Function");
-    return () => h('div', { id: 'app' }, [
-      h('h2', { onClick: log }, [
-        h('span', { class: 'material-icons', style: 'font-size: 100px' }, 
-        'whatshot'),
-        h('span', myEl.value = Date.now()),
-      ])
-    ])
+<script lang="ts" setup>
+import { ref, watch, reactive, watchEffect } from 'vue'
+const num = ref(0);
+const mesaj = ref('test');
+
+const input = () => {
+  const text = prompt()!;
+  mesaj.value = text;
+};
+
+const stopWatcher = watchEffect(() => {
+  if (num.value < 10) {
+    console.log(num.value)
+    // stopWatcher();
+  }
+}, {
+  onTrigger: () => {
+    if(num.value === 10) {
+      setTimeout(() => {
+      stopWatcher();  
+      }, 1);
+      
+    }
+    // stopWatcher();
   }
 });
+
+// watchEffect(() => {
+//   // değer değişti zaman devreye giriyor.
+//   console.log(num.value)
+// })
+
+// watch([num, mesaj], (newValues, oldValues)=> {
+//   // console.log("Num Değişti Yeni Değeri :" + num.value)
+//   // console log old value
+//   // console.log("Eski Değeri :" + oldValue)
+//   // console.log("Yeni Değeri :" + newValue)
+//   console.log({oldValues, newValues})
+// }, {
+//   immediate: true,
+//   onTrack: () => console.log("onTrack Çalıştı"),
+//   onTrigger: () => console.log("onTrigger Çalıştı")
+// }) 
 
 </script>
 
@@ -23,10 +59,6 @@ export default defineComponent({
 @import url("https://fonts.googleapis.com/icon?family=Material+Icons");
 
 .main {
-  // height: 100vh;
-  // width: 100%;
-  margin: 50px;
-
-  // flex-direction: column;
+  padding: 15%;
 }
 </style>
