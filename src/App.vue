4de67bfd7be1e8703c166b492a9b8be59a9adf92
hhakangull <hakanngul@icloud.com>
2021-12-18 18:35:36 +0300

    fff

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,37 @@
 <template>
   <div id="main">
-  <Yaz :files="files" />
+    <div class="content">
+      <div class="item">
+        <input @keydown.arrow-down="handleArrowDown" type="text" name="" id="" />
+      </div>
+      <div class="item py-2" v-for="(item, index) in items" :key="index">
+        {{ item }}
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts" setup>
 import { ref } from "vue";
-import Yaz from "./components/Yaz.vue";
-import type { Tree } from "./components/Yaz.vue";
-const files: Tree = [
-  {
-    type: "folder",
-    name: "Merhaba",
-    children: [
-      {
-        type: "file",
-        name: "asdas.mp4",
-      },
-    ],
-  },
-  {
-    type: "file",
-    name: "tasdas.ts",
-  },
-];
+const items = [...new Array(20).keys()];
+
+function handleArrowDown() {
+  console.log('handleArrowDown')
+}
+
 </script>
 
 <style>
+body{
+  background-color: black;
+  color: white;
+}
 #main {
   margin: 30px;
   padding: 30px;
 }
+.item{
+  font-size: 24px;
+}
+
 </style>
